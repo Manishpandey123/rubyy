19159324152fbc4fd48facf6fe0601e8c5a7ee3f
Manish Pandey <46836186+Manishpandey123@users.noreply.github.com>
2021-05-09 21:42:27 +0545

    changed controller/teachers

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -1,7 +1,7 @@
 class TeachersController < ApplicationController
 
     def index
-        @teachers = Teacher.all
+        @teachers = Teacher.create(Teacher_name: "ram", Teacher_address: "ktm")
     end
 
 end
