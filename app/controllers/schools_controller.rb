05dd6fde71a6b63326536997afb18089d20ae391
Manish Pandey <46836186+Manishpandey123@users.noreply.github.com>
2021-05-10 09:41:15 +0545

    ...

--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -2,6 +2,7 @@ class SchoolsController < ApplicationController
 
     def sindex
         @Schools = School.create(Teacher_name: "suman", Teacher_address: "bhaktapur")
+        @Schools.all
     end
 
 end
