2cbcbcd10bddc2d704fa0cab7b2460d90cd6565d
Manish Pandey <46836186+Manishpandey123@users.noreply.github.com>
2021-05-10 09:30:58 +0545

    ..

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,6 +2,7 @@ class StudentsController < ApplicationController
 
     def stindex
         @students = Student.create(Student_name: "manish", Teacher_address: "tinkune")
+        @students.all
     end
     
 end
--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -2,6 +2,7 @@ class TeachersController < ApplicationController
 
     def index
         @teachers = Teacher.create(Teacher_name: "ram", Teacher_address: "ktm")
+        @teachers = Teacher.all
     end
 
 end
